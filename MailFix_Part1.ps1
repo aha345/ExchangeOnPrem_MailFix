2fc011520fd6ae8ed04ddbf516c32c062cbef386
aha345 <134587220+aha345@users.noreply.github.com>
2023-05-25 09:29:17 +0200

    Add files via upload

---
+++ MailFix_Part1.ps1
@@ -0,0 +1,241 @@
+#####################################################################
+#                                                                   #
+#                                                                   #
+#                       Script created by                           #
+#                                                                   #
+#                              Kenkot                               #
+#                                &                                  #
+#                              Anderi                               #
+#                                                                   #
+#                               #AMS                                #
+#                                                                   #
+#                                                                   #
+#####################################################################
+
+Get-Variable |
+    Where-Object { $_.Name -ne "Credential" -and $_.Options -ne "ReadOnly" -and $_.Options -ne "AllScope" -and $_.Options -ne "Constant" } |
+    Remove-Variable -ErrorAction SilentlyContinue
+
+function Prompt-UserForPrimaryAddress {
+    param(
+        [Parameter(Mandatory = $true)] [string[]] $Addresses,
+        [Parameter(Mandatory = $true)] [string] $AddressType
+    )
+
+    $menuItems = $Addresses | ForEach-Object { New-Object PSObject -Property @{ Label = $_ } }
+    $title = "Please select the primary $AddressType to keep:"
+    $menu = $($menuItems | ForEach-Object { "{0}. {1}" -f ($_.ReadCount, $_.Label) }) -join "`n"
+
+    do {
+        $choice = Read-Host -Prompt "$title`n$menu`n`nPlease enter the number:"
+        $selectedAddress = $menuItems[$choice - 1].Label
+    } until ($selectedAddress)
+
+    return $selectedAddress
+}
+
+# Check if AzureAD credentials are already stored
+if (-not $Credential) {
+    # Prompt the user for AzureAD credentials
+    $Credential = Get-Credential -Message "Enter your AzureAD credentials"
+}
+
+# Connect to AzureAD with the provided credentials
+Connect-AzureAD -Credential $Credential | Out-Null
+
+# Connect to Exchange Online with the provided credentials
+$EXOConnectionUri = "https://outlook.office365.com/powershell-liveid/"
+Connect-ExchangeOnline -Credential $Credential -ConnectionUri $EXOConnectionUri -ShowBanner:$false
+
+# Clear the console window
+Clear-Host
+
+# What AzureAD Tenant is connected
+$AzureADTenant = ((Get-AzureADTenantDetail).VerifiedDomains | Where-Object { $_.Name -like "*.onmicrosoft.com" -and $_.Name -notlike "*.mail.onmicrosoft.com" } | Select-Object -ExpandProperty Name) -replace '.onmicrosoft.com', ''
+$(Write-Host "Connected to AzureAD Tenant: " -ForegroundColor Cyan -NoNewLine) + $(Write-Host "$AzureADTenant" -ForegroundColor Green)
+
+# Prompt the user for the username of the AD user to modify
+$Username = $(Write-Host "Enter the username of the AD user to modify " -ForegroundColor Magenta -NoNewLine) + $(Write-Host "(Can be Name, UserPrincipalName, Email, or SamAccountName)" -ForegroundColor Yellow -NoNewLine) + $(Write-Host ": " -ForegroundColor Magenta -NoNewLine; Read-Host)
+
+# Find the AD user based on the provided username and retrieve all properties
+$ADUser = Get-ADUser -Filter {
+    (Name -eq $Username) -or
+    (UserPrincipalName -eq $Username) -or
+    (EmailAddress -eq $Username) -or
+    (SamAccountName -eq $Username)
+} -Properties *
+
+# Get the UPN of the AD user
+$UPN = $ADUser.UserPrincipalName
+
+# Get the specified AD attributes for the user
+$Name = $ADUser.Name
+$SamAccountName = $ADUser.SamAccountName
+$Mail = $ADUser.Mail
+$TargetAddress = $ADUser.TargetAddress
+$msexchRecipientDisplayType = $ADUser.msexchRecipientDisplayType
+$TypeDetails = $ADUser.msExchRecipientTypeDetails
+$ADProxyAddresses = $ADUser.ProxyAddresses
+
+# Find the user in Azure AD based on the UPN and retrieve all properties
+$AzureADUser = Get-AzureADUser -ObjectId $UPN -ErrorAction SilentlyContinue
+
+# Find the user in Exchange Online based on the UPN and retrieve all properties
+$EXOUser = Get-Mailbox -Identity $UPN -ErrorAction SilentlyContinue -WarningAction SilentlyContinue
+
+# Get the proxy addresses of the AzureAD user
+if ($AzureADUser) {
+    $AzureADProxyAddresses = $AzureADUser.ProxyAddresses
+}
+
+# Get the proxy addresses of the Exchange Online user
+if ($EXOUser) {
+    $EXOProxyAddresses = $EXOUser.EmailAddresses
+    $ExchangeGuid = $EXOUser.ExchangeGuid
+}
+
+# Print the current date and time
+Write-Host "Current date and time: $(Get-Date)"
+
+# Write user info
+$(Write-Host "Name:" -ForegroundColor Cyan) + $(Write-Host "$Name" -ForegroundColor DarkGray)
+$(Write-Host "Mail:" -ForegroundColor Cyan) + $(Write-Host "$Mail" -ForegroundColor DarkGray)
+$(Write-Host "SamAccountName:" -ForegroundColor Cyan) + $(Write-Host "$SamAccountName" -ForegroundColor DarkGray)
+$(Write-Host "TargetAddress:" -ForegroundColor Cyan) + $(Write-Host "$TargetAddress" -ForegroundColor DarkGray)
+
+# Print all ProxyAddresses with duplicates removed
+$AllProxyAddresses = @($ADProxyAddresses) + @($AzureADProxyAddresses) + @($EXOProxyAddresses)
+$AllProxyAddresses = $AllProxyAddresses | Sort-Object | Select-Object -Unique
+Write-Host "All ProxyAddresses:" -ForegroundColor Cyan
+foreach ($Address in $AllProxyAddresses) {
+    Write-Host $Address -ForegroundColor DarkGray
+}
+
+# Check for duplicate all caps SMTP or X500 values
+$SMTPPrimaryAddresses = $AllProxyAddresses | Where-Object { $_ -clike "SMTP:*" }
+if ($SMTPPrimaryAddresses.Count -gt 1) {
+    Write-Warning "There are multiple primary SMTP values in the ProxyAddresses:"
+    $SMTPPrimaryAddresses | ForEach-Object {
+        Write-Warning $_
+    }
+
+    $primarySMTPToKeep = Prompt-UserForPrimaryAddress -Addresses $SMTPPrimaryAddresses -AddressType "SMTP"
+    $AllProxyAddresses = $AllProxyAddresses | ForEach-Object {
+        if ($_ -clike "SMTP:*" -and $_ -cne $primarySMTPToKeep) {
+            "smtp:$($_.Substring(5))"
+        } else {
+            $_
+        }
+    }
+}
+
+$X500PrimaryAddresses = $AllProxyAddresses | Where-Object { $_ -clike "X500:*" }
+if ($X500PrimaryAddresses.Count -gt 1) {
+    Write-Warning "There are multiple primary X500 values in the ProxyAddresses:"
+    $X500PrimaryAddresses | ForEach-Object {
+        Write-Warning $_
+    }
+
+    $primaryX500ToKeep = Prompt-UserForPrimaryAddress -Addresses $X500PrimaryAddresses -AddressType "X500"
+    $AllProxyAddresses = $AllProxyAddresses | ForEach-Object {
+        if ($_ -clike "X500:*" -and $_ -cne $primaryX500ToKeep) {
+            "x500:$($_.Substring(5))"
+        } else {
+            $_
+        }
+    }
+}
+
+# Add onmicrosoft addresses if they don't exist
+if ($AzureADTenant) {
+    $onmicrosoftAddress = "$SamAccountName@$AzureADTenant.onmicrosoft.com"
+    $mailOnmicrosoftAddress = "$SamAccountName@$AzureADTenant.mail.onmicrosoft.com"
+    $onmicrosoftExists = $AllProxyAddresses | Where-Object { $_ -like "*$AzureADTenant.onmicrosoft.com" }
+    $mailOnmicrosoftExists = $AllProxyAddresses | Where-Object { $_ -like "*$AzureADTenant.mail.onmicrosoft.com" }
+    
+    if (-not $onmicrosoftExists) {
+        $AllProxyAddresses += $onmicrosoftAddress
+    }
+    if (-not $mailOnmicrosoftExists) {
+        
+        # Create a variable for the existing mail.onmicrosoft.com address
+        if ($MailOnMicrosoftAddress -notlike "smtp:*$AzureADTenant.mail.onmicrosoft.com") {
+            $SMTPMailOnMicrosoftAddress = $MailOnMicrosoftAddress.Insert(0,"smtp:")
+            $AllProxyAddresses += $SMTPMailOnmicrosoftAddress
+        }
+    
+        else {
+            $AllProxyAddresses += $mailOnmicrosoftAddress
+        }
+    }
+}
+
+# Add mail.onmicrosoft.com address to ExistingMailOnMicrosoftAddress
+if ($MailOnMicrosoftAddress -like "smtp:*$AzureADTenant.mail.onmicrosoft.com") {
+
+    $ExistingMailOnMicrosoftAddress = $MailOnMicrosoftAddress -Replace '[smtp:]',''
+
+} else {
+
+    $ExistingMailOnMicrosoftAddress = $MailOnMicrosoftAddress
+
+}
+
+# Remove duplicates
+$AllProxyAddresses = $AllProxyAddresses | Select-Object -Unique
+
+# Create new variable for all sorted ProxyAddresses with duplicates removed
+$AllSortedProxyAddresses = $AllProxyAddresses | Sort-Object @{
+    Expression = {
+        if ($_ -like "SMTP:*" -or $_ -like "X500:*") {
+            "0" + $_.ToUpper()
+        } else {
+            "1" + $_
+        }
+    }
+} | Select-Object -Unique
+
+
+# Create an object with the values to export
+$ExportObject = [PSCustomObject]@{
+    Name = $Name
+    UPN = $UPN
+    SAMAccountName = $SamAccountName
+    Mail = $Mail
+    TargetAddress = $TargetAddress
+    msexchRecipientDisplayType = $msexchRecipientDisplayType
+    msExchRecipientTypeDetails = $TypeDetails
+    ExchangeGuid = $ExchangeGuid
+    AllSortedProxyAddresses = $AllSortedProxyAddresses -join ', '
+    ExistingMailOnMicrosoftAddress = $ExistingMailOnMicrosoftAddress
+}
+
+# Specify the path to the output CSV file
+$OutputFileName = "$($env:USERPROFILE)\Desktop\$(Get-Date -Format 'yyyyMMdd')-$($ADUser.Name).csv"
+
+# Export the object to a CSV file
+$ExportObject | Export-Csv -Path $OutputFileName -Encoding UTF8 -NoTypeInformation
+
+# Get the current user's distinguished name (DN) 
+$currentuser = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
+$username = $currentuser.split('\')[-1]
+
+# Disconnect for Azure AD
+Disconnect-AzureAD
+
+# Disconnect Exchange Online
+function Disconnect-ExchangeOnlineSilent {
+    & {
+        Disconnect-ExchangeOnline -Confirm:$false
+    } 6>&1 > $null
+}
+
+Disconnect-ExchangeOnlineSilent
+
+# Check if the user is a member of the "Organization Management" group 
+$orgmanager = Get-ADGroupMember -Identity "Organization Management" -Recursive | where {$_.SamAccountName -eq $username} 
+ 
+if ($orgmanager) { 
+} else { 
+    Write-Host "$username is not a member of the Organization Management group" -ForegroundColor Red
+}
---
+++ MailFix_Part2.ps1
@@ -0,0 +1,290 @@
+#####################################################################
+#                                                                   #
+#                                                                   #
+#                       Script created by                           #
+#                                                                   #
+#                              Kenkot                               #
+#                                &                                  #
+#                              Anderi                               #
+#                                                                   #
+#                               #AMS                                #
+#                                                                   #
+#                                                                   #
+#####################################################################
+
+
+function Import-CsvFile {
+    param(
+        [string] $DefaultPath = "$($env:USERPROFILE)\Desktop",
+        [string] $DefaultFileName = "$(Get-Date -Format 'yyyyMMdd')-$($ADUser.Name).csv"
+    )
+
+    do {
+        $filename = Read-Host -Prompt "Please enter the filename (default: $DefaultFileName):"
+        if (-not $filename) { $filename = $DefaultFileName }
+        $filepath = Join-Path -Path $DefaultPath -ChildPath $filename
+        if (-not (Test-Path $filepath)) {
+            Write-Warning "File not found. Please try again."
+        }
+    } until (Test-Path $filepath)
+
+    return Import-Csv -Path $filepath
+}
+
+# Define the date format
+$dateFormat = "dd.MM.yyyy HH.mm.ss"
+
+# Get the current date and time
+$currentDate = Get-Date
+
+# Clear the console window
+Clear-Host
+
+# Add some variables we'll use later
+$CurrentHostname = hostname
+
+# Check if the user running the script has access to the on-premises PowerShell environment
+try {
+    Add-PSSnapin -Name Microsoft.Exchange.Management.PowerShell.SnapIn -ErrorAction Stop
+}
+catch {
+    Write-Warning "You don't have access to the on-premises PowerShell environment."
+    Start-Sleep -Seconds 10
+    exit
+}
+
+# Import the CSV file
+$importedData = Import-CsvFile
+
+# Access the data from the CSV file
+foreach ($item in $importedData) {
+    $Name = $item.Name
+    $UserUPN = $item.UPN
+    $SAMAccountName = $item.SAMAccountName
+    $Mail = $item.Mail
+    $TargetAddress = $item.TargetAddress
+    $msexchRecipientDisplayType = $item.msexchRecipientDisplayType
+    $msExchRecipientTypeDetails = $item.msExchRecipientTypeDetails
+    $ExchangeGuid = $item.ExchangeGuid
+    $AllSortedProxyAddresses = $item.AllSortedProxyAddresses -split ', '
+    $ExistingMailOnMicrosoftAddress = $item.ExistingMailOnMicrosoftAddress
+}
+
+# Check if $item.mail is blank
+if ($Mail -notlike '*@*'){
+    $Mail = $UserUPN
+}
+
+# Check if $targetaddress is blank
+if ($TargetAddress -notlike '*@*'){
+    $TargetAddress = $ExistingMailOnMicrosoftAddress.Insert(0,"SMTP:")
+}
+
+# Prompt the user to select mailbox type
+$MailboxType = $(Write-Host "Is the mailbox a 'User', 'Shared', 'Room' or 'Equipment' mailbox?" -ForegroundColor Magenta -NoNewLine) + $(Write-Host "(Type 'User', 'Shared', 'Room' or 'Equipment'.)" -ForegroundColor Yellow -NoNewLine) + $(Write-Host ": " -ForegroundColor Magenta -NoNewLine; Read-Host)
+
+# Check if a recipient object (such as a mailbox) exists for the given UPN, Email, or Alias
+$Mailbox = $null
+try {
+    if (-not $Mailbox) {
+        $Mailbox = Get-Recipient -ResultSize 1 -Filter "UserPrincipalName -eq '$UserUPN' -or EmailAddresses -like 'SMTP:$Mail' -or Alias -eq '$SAMAccountName'" -ErrorAction Stop
+        Start-Sleep -Seconds 5
+    }
+} catch {
+    Write-Warning "Get-Recipient can't find mailbox"
+}
+
+# Check if the mailbox exists
+if ($Mailbox) {
+    $CurrentRecipientType = $Mailbox.RecipientType
+    $(Write-Host "Mailbox for UPN '$UserUPN' exists with RecipientType: " -ForegroundColor Gray -NoNewLine) + $(Write-Host "$($Mailbox.RecipientTypeDetails)" -ForegroundColor Green)
+} else {
+    Write-Warning "Mailbox for UPN '$UserUPN' does not exist"
+}
+
+# Get mailbox size
+$MailboxStatistics = Get-MailboxStatistics -Identity $UserUPN -ErrorAction SilentlyContinue
+$MailboxSize = $MailboxStatistics.TotalItemSize
+
+if ($MailboxStatistics) {
+    # Output mailbox size
+    Write-Host "Mailbox size: $($MailboxSize.ToString())" -ForegroundColor Red
+    $(Write-Host "$($UserUPN) has an " -ForegroundColor Cyan -NoNewLine) + $(Write-Host "On-Prem Mailbox" -ForegroundColor Yellow)
+} else {
+    $(Write-Host "$($UserUPN) does not have an " -ForegroundColor Green -NoNewLine) + $(Write-Host "On-Prem Mailbox" -ForegroundColor Yellow)
+}
+
+# Check if the mailbox exists
+if ($MailboxStatistics) {
+    # Prompt user to confirm mailbox deletion
+    $confirmation = $(Write-Host "Do you want to delete the on-prem mailbox for $($UserUPN)?" -ForegroundColor Magenta -NoNewLine) + $(Write-Host "(Type 'Yes' to confirm, or any other key to skip)" -ForegroundColor Yellow -NoNewLine) + $(Write-Host ": " -ForegroundColor Magenta -NoNewLine; Read-Host)
+
+    if ($confirmation -eq "Yes") {
+        # Get the current user's distinguished name (DN) 
+        $currentuser = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
+        $username = $currentuser.split('\')[-1]
+
+        # Check if the user has the "Mailbox Import Export" role
+        $roleName = "Mailbox Import Export"
+        $hasRole = (Get-ManagementRoleAssignment -Role $roleName -RoleAssignee $userName -RoleAssigneeType User -ErrorAction SilentlyContinue) -ne $null
+
+        $null = Add-MailboxPermission -Identity $UserUPN -User $username -AccessRights FullAccess -InheritanceType All -ErrorAction SilentlyContinue -WarningAction SilentlyContinue
+
+        Start-Sleep -Seconds 5
+
+        if ($hasRole) {
+        } else {
+            # Assign the "Mailbox Import Export" role to the user
+            New-ManagementRoleAssignment -Role $roleName -User $userName
+            Write-Host "The '$roleName' role has been assigned to the user '$userName'." -ForegroundColor Yellow
+            Start-Sleep -Seconds 5
+        }
+
+        # Prompt user to confirm mailbox export
+        $ConfirmExport = $(Write-Host "Do you want to export the on-prem mailbox for $($UserUPN)? " -ForegroundColor Magenta -NoNewLine) + $(Write-Host "(Type 'Yes' to confirm, or any other key to skip)" -ForegroundColor Yellow -NoNewLine) + $(Write-Host ": " -ForegroundColor Magenta -NoNewLine; Read-Host)
+
+        if ($ConfirmExport -eq "Yes") {
+            $(Write-Host "") + $(Write-Host "Open browser and go to the following address to export the mailbox: localhost/ecp" -ForegroundColor Yellow) + $(Write-Host "")
+            Start-Sleep -Seconds 5
+
+            # Path for export mailbox content to a PST file
+            $PSTPath = "\\$CurrentHostname\$env:UserName\Desktop\$(Get-Date -Format 'yyyyMMdd')-$UserUPN.pst"
+            $(Write-Host "The following path is needed for export of the .pst file:" -ForegroundColor Yellow) + $(Write-Host "") + $(Write-Host "$($PSTPath)" -ForegroundColor Cyan)
+            
+            # Promt user to add path to clipboard
+            $ConfirmClip = $(Write-Host "Do you want to copy the path to clipboard? " -ForegroundColor Magenta -NoNewLine) + $(Write-Host "(Type 'Yes' to confirm, or any other key to skip)" -ForegroundColor Yellow -NoNewLine) + $(Write-Host ": " -ForegroundColor Magenta -NoNewLine; Read-Host)
+            if ($ConfirmClip -eq "Yes") {
+                $PSTPath | clip.exe
+            }
+
+            # Check if mailbox is exported
+            $Exported = $false
+            while (-not $Exported) {
+                try {
+                    $lastDay = $currentDate.AddDays(-7)
+                    $ExportStatus = Get-MailboxExportRequest -Mailbox $SamAccountName | Where-Object {$_.WhenCreated -gt $lastDay}
+
+                    do {
+                        $stats = Get-MailboxExportRequestStatistics -Identity $SamAccountName -WarningAction SilentlyContinue
+                        Write-Warning "Export status: $($stats.Status), Percent complete: $($stats.PercentComplete)!"
+                        Start-Sleep -Seconds 5
+                    } while ($stats.Status -eq "Queued" -or $stats.Status -eq "InProgress")
+
+                    if ($ExportStatus.status -eq "Completed"){
+                        $Exported = $true
+                        Write-Host ""
+                        Write-Host "Exchange has completed the export of the mailbox" -ForegroundColor Green
+                    }
+                }
+                catch {
+                    Start-Sleep -Seconds 5
+                    
+                }
+            }
+
+            if ($Exported = $true) {
+                # Prompt user to confirm disabling of mailbox
+                $ConfirmDisable = $(Write-Host "Do you want to disable the on-prem mailbox for $($UserUPN)? " -ForegroundColor Magenta -NoNewLine) + $(Write-Host "(Type 'Yes' to confirm, or any other key to skip)" -ForegroundColor Yellow -NoNewLine) + $(Write-Host ": " -ForegroundColor Magenta -NoNewLine; Read-Host)
+
+                if ($ConfirmDisable = "Yes") {
+                    # Disable the on-prem mailbox
+                    Disable-Mailbox -Identity $Mailbox.PrimarySmtpAddress -Confirm:$false
+                    Start-Sleep -Seconds 5
+                    Write-Host "The on-prem mailbox for $($UserUPN) has been disabled." -ForegroundColor Yellow
+                    Start-Sleep -Seconds 5
+                }
+            }           
+        }
+    } else {
+        Write-Warning "Skipping on-prem mailbox deletion for $($UserUPN)."
+    }
+} else {
+    Write-Warning "No on-prem mailbox found for $($UserUPN)."
+}
+
+# Get the current values of the AD user fields
+$ADUser = Get-ADUser -Identity $SAMAccountName -Properties mail, TargetAddress, ProxyAddresses, msExchRecipientDisplayType, msExchRecipientTypeDetails
+
+# Prompt user to confirm blanking the fields
+$confirmationBlank = $(Write-Host "Do you want to blank Exchange properties for $($UserUPN)?" -ForegroundColor Magenta -NoNewLine) + $(Write-Host "(Type 'Yes' to confirm, or any other key to skip)" -ForegroundColor Yellow -NoNewLine) + $(Write-Host ": " -ForegroundColor Magenta -NoNewLine; Read-Host)
+
+if ($confirmationBlank -eq "Yes") {
+    # Blank the fields using Set-ADUser
+    Set-ADUser -Identity $SAMAccountName -Clear mail, TargetAddress, ProxyAddresses, msExchRecipientDisplayType, msExchRecipientTypeDetails
+    Write-Host "Fields have been blanked for $($UserUPN)." -ForegroundColor Gray
+} else {
+    Write-Host "Skipping blanking fields for $($UserUPN)." -ForegroundColor Cyan
+}
+
+# Get the updated values of the AD user fields
+$UpdatedADUser = Get-ADUser -Identity $SAMAccountName -Properties mail, TargetAddress, ProxyAddresses, msExchRecipientDisplayType, msExchRecipientTypeDetails
+
+# Initialize variables
+$ADmsExchRecipientDisplayType = 0
+$ADmsExchRecipientTypeDetails = 0
+
+# Set variables based on mailbox type
+if ($MailboxType -eq "User") {
+    $ADmsExchRecipientDisplayType = 1073741824
+    $ADmsExchRecipientTypeDetails = 1
+} elseif ($MailboxType -eq "Shared") {
+    $ADmsExchRecipientDisplayType = 0
+    $ADmsExchRecipientTypeDetails = 4
+}elseif ($MailboxType -eq "Room") {
+    $ADmsExchRecipientDisplayType = 7
+    $ADmsExchRecipientTypeDetails = 16
+}elseif ($MailboxType -eq "Equipment") {
+    $ADmsExchRecipientDisplayType = 8
+    $ADmsExchRecipientTypeDetails = 32
+} else {
+    $(Write-Host "Invalid mailbox type." -ForegroundColor Red -NoNewLine) + $(Write-host "Please type 'User', 'Shared', 'Room' or 'Equipment'." -ForegroundColor Yellow)
+    exit
+}
+
+# Set the mail, ProxyAddresses, msExchRecipientDisplayType, and msExchRecipientTypeDetails fields
+Set-ADUser -Identity $SAMAccountName -Replace @{
+        mail = $Mail;
+        ProxyAddresses = $AllSortedProxyAddresses;
+        msExchRecipientDisplayType = $ADmsExchRecipientDisplayType;
+        msExchRecipientTypeDetails = $ADmsExchRecipientTypeDetails
+    }
+
+# Output a message to indicate the fields have been updated
+Write-Host "The mail, ProxyAddresses, msExchRecipientDisplayType, and msExchRecipientTypeDetails fields have been updated for $($UserUPN)." -ForegroundColor Gray
+
+# Enable the remote mailbox
+try {
+    $EnableRemoteMailBox = Enable-RemoteMailbox -Identity "$Mail" -RemoteRoutingAddress "$ExistingMailOnMicrosoftAddress" -ErrorAction Stop
+} catch {
+    Write-Host "Error: $_" -ForegroundColor Red
+}
+
+# Set the Exchange GUID for the remote mailbox
+$RecipientTypeDetails = Get-RemoteMailbox -Identity $Mail -ErrorAction SilentlyContinue -WarningAction SilentlyContinue | where {$_.RecipientTypeDetails -ne "RemoteuserMailbox"}
+
+if ($RecipientTypeDetails.RecipientTypeDetails -ne "Remote*Mailbox") {
+    $success = $false
+    while (-not $success) {
+        try {
+            Set-RemoteMailbox -Identity $SamAccountName -ExchangeGuid $ExchangeGuid -ErrorAction Stop
+            $success = $true
+        } catch {
+            $(Write-Host "An error occurred while setting RemoteMailbox: " -ForegroundColor Red -NoNewLine) + $(Write-Host "Retrying in 5 seconds..." -ForegroundColor Yellow)
+            Start-Sleep -Seconds 5
+        }
+    }
+} else {
+    Write-Host "RemoteMailbox already set" -ForegroundColor Yellow
+}
+
+# Get the updated TargetAddress value of the AD user
+$UpdatedADUser = Get-ADUser -Identity $SAMAccountName -Properties TargetAddress
+
+# Check if the TargetAddress field is populated
+if ([string]::IsNullOrEmpty($UpdatedADUser.TargetAddress)) {
+    # Set the TargetAddress field using Set-ADUser
+    Set-ADUser -Identity $SAMAccountName -Replace @{TargetAddress = $TargetAddress}
+    Write-Host "The TargetAddress field for $($UserUPN) has been set to $($TargetAddress)." -ForegroundColor Gray
+} else {
+    Write-Host "The TargetAddress field for $($UserUPN) was unchanged." -ForegroundColor Yellow
+}
